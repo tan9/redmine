d4faeccd5af09b9e252345084ca2720b3fdb39d2
Pei-Tang Huang <tangtheone@gmail.com>
2016-04-13 13:59:28 +0800

    Add redmine-tags plugin.

--- 3.2/docker-entrypoint.sh
+++ 3.2/docker-entrypoint.sh
@@ -63,6 +63,7 @@ case "$1" in
 		fi
 		if [ "$1" != 'rake' -a -z "$REDMINE_NO_DB_MIGRATE" ]; then
 			gosu redmine rake db:migrate
+			gosu redmine rake redmine:plugins:migrate NAME=redmine_tags RAILS_ENV=production
 		fi
 		
 		chown -R redmine:redmine files log public/plugin_assets
